85575a2006afffafe2259c925bb49bfef43356c8
tty128 <ccggaag.ffeeddc.128@gmail.com>
2021-08-09 14:44:35 +0900

    css

--- app/pages/taxonomy/_taxonomy/_term/index.vue
+++ app/pages/taxonomy/_taxonomy/_term/index.vue
@@ -5,7 +5,7 @@
     </h2>
     <div v-if="data.length > 0" class="wrapper">
       <MoleculesPagination :pages="data" button-class="cl-secoundary" />
-      <OrganismsCardSort :data="dataSlice(data,parseInt($route.query.page || 1))" />
+      <OrganismsCardSort class="blog-list__items flex--s" :data="dataSlice(data,parseInt($route.query.page || 1))" />
       <MoleculesPagination :pages="data" button-class="cl-secoundary" />
     </div>
     <p v-else class="flex--c">
@@ -32,4 +32,18 @@ export default class TaxonomyTermComponent extends Vue {
 </script>
 
 <style lang="scss">
+.blog-list {
+  &__items {
+    @media screen and (max-width:480px)  {
+      justify-content: space-between !important;
+
+      > li {
+        margin: 0 0 2rem!important;
+        width: 42.5vw !important;
+        height: 60vw !important;
+        font-size: 0.8em !important;
+      }
+    }
+  }
+}
 </style>
