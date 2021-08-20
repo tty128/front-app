8ded90fffa92adb187f5aeb5a8186fe2f36ccc77
tty128 <83931052+tty128@users.noreply.github.com>
2021-08-20 16:45:58 +0900

    Update Rainy.ts

--- app/types/animation/main/Rainy.ts
+++ app/types/animation/main/Rainy.ts
@@ -1,13 +1,10 @@
 /* eslint-disable no-dupe-class-members */
 import Rain from '../objects/Rain'
+import Umbrella from '../objects/Umbrella'
 import Point from '../src/interface/Point'
 import AnimationLayerMain from '../src/main/AnimationLayerMain'
 
 export default class Rainy extends AnimationLayerMain {
-  private isMouseover : boolean = false
-  private mouseoverColor : string = '156, 204, 101'
-  private mousePoint : Point = { x: 0, y: 0 }
-  private counter : number = 0
   protected randomColor: string[] | null = [
     '210, 210, 210',
     '255, 255, 255',
@@ -15,7 +12,7 @@ export default class Rainy extends AnimationLayerMain {
     '170, 242, 196'
   ]
 
-  private probability : number = 12
+  private probability : number = 15
 
   public eventAciton (event?: string | number, params?: Point): void {
     let x : number = 0
@@ -50,40 +47,29 @@ export default class Rainy extends AnimationLayerMain {
         const py = y + absY / 2 - this.random(absY)
         this.add(new Rain([px, py], { speed: 3 }))
         this.add(new Rain([px, py], { speed: 3 }), { delay: 10 })
-      } else if (event === 'navigation' || event === 'pagination') {
-        this.mousePoint = { x, y }
-        this.mouseoverColor = '156, 204, 101'
-        this.isMouseover = true
-      } else if (event === 'mouseout') {
-        this.isMouseover = false
-      } else if (event === 'pagination--current') {
-        this.mousePoint = { x, y }
-        this.mouseoverColor = '255, 138, 101'
-        this.isMouseover = true
-      } else if (event === 'navigation--current') {
-        this.mousePoint = { x, y }
-        this.mouseoverColor = '255, 255, 255'
-        this.isMouseover = true
       }
     }
   }
 
-  public init (): void {}
+  public init (): void {
+    this.addLayer(1)
+  }
 
+  private umbrella : boolean = true
   public paint (_ctx : CanvasRenderingContext2D) : void {
-    const prob : number = this.getCanvas().width / 1500 * this.probability
     const accuracy = 1000
     const rand : number = this.random(accuracy)
-    if (rand > accuracy * (1 - (prob / 100))) {
+    if (rand > accuracy * (1 - (this.probability / 100))) {
       const randX : number = this.random(this.getCanvas().width)
       const randY : number = this.random(this.getCanvas().height)
       this.add(new Rain([randX, randY]))
     }
 
-    if (this.isMouseover && this.counter++ > 40) {
-      const op = { speed: 2, lineWidth: 15, random: false }
-      this.add(new Rain([this.mousePoint.x!, this.mousePoint.y!], op), { color: this.mouseoverColor })
-      this.counter = 0
+    this.changeFocusLayer(1)
+    if (this.umbrella) {
+      this.add(new Umbrella([], { size: 70, speed: 0.5, reactive: { x: 90, y: 75 } }))
+      this.umbrella = false
     }
+    this.changeFocusLayer(0)
   }
 }
